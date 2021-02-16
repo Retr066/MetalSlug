ea11890bf5c0c0ab46258ff7db2d9bb303bcdad5
JhersonLopez <jherson.lopez88@gmail.com>
2021-02-15 22:11:15 -0500

    first commit

---
+++ js/evento.js
@@ -0,0 +1,41 @@
+
+const sonidos = document.getElementById("1");
+sonidos.addEventListener("click", function () {
+  sonidos.innerHTML = `<audio src="./sounds/H.mp3" autoplay></audio>`
+   
+  });
+  const sonidos1 = document.getElementById("2");
+  sonidos1.addEventListener("click", function () {
+    sonidos1.innerHTML = `<audio src="./sounds/R.mp3" autoplay></audio>`
+    });
+
+    const sonidos3 = document.getElementById("3");
+    sonidos3.addEventListener("click", function () {
+      sonidos3.innerHTML = `<audio src="./sounds/F.mp3" autoplay></audio>`
+      });
+      const sonidos4 = document.getElementById("4");
+  sonidos4.addEventListener("click", function () {
+    sonidos4.innerHTML = `<audio src="./sounds/L.mp3" autoplay></audio>`
+    });
+    const sonidos5 = document.getElementById("5");
+  sonidos5.addEventListener("click", function () {
+    sonidos5.innerHTML = `<audio src="./sounds/S.mp3" autoplay></audio>`
+    });
+    const sonidos6 = document.getElementById("6");
+  sonidos6.addEventListener("click", function () {
+    sonidos6.innerHTML = `<audio src="./sounds/C.mp3" autoplay></audio>`
+    });
+    const sonidos7 = document.getElementById("7");
+  sonidos7.addEventListener("click", function () {
+    sonidos7.innerHTML = `<audio src="./sounds/I.mp3" autoplay></audio>`
+    });
+    const sonidos8 = document.getElementById("8");
+  sonidos8.addEventListener("click", function () {
+    sonidos8.innerHTML = `<audio src="./sounds/D.mp3" autoplay></audio>`
+    });
+    const sonidos9 = document.getElementById("9");
+    sonidos9.addEventListener("click", function () {
+      sonidos9.innerHTML = `<audio src="./sounds/G.mp3" autoplay></audio>`
+      });
+
+      
